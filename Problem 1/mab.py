18ea54d9fdc12ef37a6868d17b8611ee46221b77
Runar Fosse <rfosse01@gmail.com>
2024-01-23 11:46:45 +0100

    Implemented both Epsilon-greedy MABs

--- Problem 1/mab.py
+++ Problem 1/mab.py
@@ -1,4 +1,7 @@
 from abc import abstractmethod
+import random
+
+import numpy as np
 
 class MAB():
     """ Multi Armed Bandit superclass """
@@ -6,3 +9,35 @@ class MAB():
     def sample(self):
         """ Call to retrieve action """
         pass
+    
+    @abstractmethod
+    def update(self):
+        """ Call to update model """
+        pass
+
+
+class EpsilonGreedy(MAB):
+    def __init__(self, epsilon=0.3, alpha=1, actions=3):
+        super().__init__()
+
+        self.expected_reward = [ 1000 ] * actions
+        self.action_counters = [ 0 ] * actions
+        self.epsilon = epsilon
+        self.actions = actions
+
+        # Optional Decaying Epsilon-greedy parameter (Default no decay)
+        self.alpha = alpha
+
+    def sample(self):
+        e = random.random()
+        if e < self.epsilon:
+            action = random.randint(0, self.actions - 1)
+        else:
+            action = np.argmax(self.expected_reward)
+        
+        return action
+        
+    def update(self, action, reward, timestep):
+        self.action_counters[action] += 1
+        self.expected_reward[action] += 1 / timestep * (reward - self.expected_reward[action])
+        self.epsilon *= self.alpha
